aca1b9bdacd3d23243a4e4c653fd7b8e5dc46848
desenvolvedor <eco_dias@yahoo.com.br>
2021-03-02 17:32:28 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppTest/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTest.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Despesa")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Despesa deve ser maior que 0")]
+        [DisplayName("Valor")]
+        public int Amount { get; set; }
+    }
+}
---
+++ WebAppTest/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTest.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Pegou Emprestado")]
+        public string Borrower { get; set; }
+        [DisplayName("Emprestou")]
+        public string Lender { get; set; }
+        [DisplayName("Ítem")]
+        public string ItemName { get; set; }
+    }
+}
